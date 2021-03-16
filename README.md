600885403b922bed81f7fb8948b3d3e622e3b9a9
Andy1184 <53714302+Andy1184@users.noreply.github.com>
2021-03-16 21:52:11 +1100

    Updated README and added handshake
    Updated README to reflect findings
    
    Added code to allow handshake between server and client to occur (HELO, OK, AUTH)

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # COMP3100-Assignment-1
 DS-Sim Client-Server 
 
+--------------------------------------------------------------------------------------------------------------------
+SERVER
 
-./ds-sim -n
+"ds-config01.xml" (or other config file) must be present in the same folder as the ds-server executable file
+Command:
+./ds-server -v all -c ds-config01.xml
+
+--------------------------------------------------------------------------------------------------------------------
+CLIENT
+
+"ds-system.xml" must be present in the same folder as the ds-client executable file
+Commands:
+(1): javac client.java
+(2): java client -a bf -c ds-system.xml
+
+--------------------------------------------------------------------------------------------------------------------
--- client.java
+++ client.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class client {
     public static void main(String[] args) {
@@ -8,18 +9,33 @@ public class client {
             Socket s = new Socket("localhost", 50000);
             DataInputStream dis = new DataInputStream(s.getInputStream());
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF("HELO\n");
+            
+            //Make a string, convert it to an array of bytes then
+            //send a bytestream to the server
+            String myString = "HELO";
+            byte[] byteArray = myString.getBytes();
+            dout.write(byteArray);
             dout.flush();
-            String str = (String) dis.readUTF();
-            System.out.println("message= " + str);
-            dout.writeUTF("AUTH");
+
+            //Read the bytestream from the server
+            byteArray = new byte[dis.available()];
+            dis.read(byteArray);
+            //Make a string using the recieved bytes and print it
+            myString = new String(byteArray, StandardCharsets.UTF_8);
+            System.out.println(myString);
+            
+            //Authenticate with a username (ubuntu)
+            myString = "AUTH ubuntu";
+            byteArray = myString.getBytes();
+            dout.write(byteArray);
             dout.flush();
-            str = (String) dis.readUTF();
-            System.out.println("message= " + str);
-            dout.writeUTF("AUTH");
+            //Tells the server that the client wants to quit
+            myString = "QUIT";
+            byteArray = myString.getBytes();
+            dout.write(byteArray);
             dout.flush();
-            //str = (String) dis.readUTF();
-           // System.out.println("message= " + str);
+
+            //Closes Session
             dout.close();
             s.close();
         } catch (Exception e) {
