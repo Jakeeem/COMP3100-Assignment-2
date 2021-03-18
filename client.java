79974bda132f5335b49189317c7ea903af7c66b2
Andy1184 <53714302+Andy1184@users.noreply.github.com>
2021-03-18 21:24:42 +1100

    Client now asks for jobs and schedules some jobs
    Client now asks for jobs and schedules them (Randomly and mostly to unavailable servers)

--- client.java
+++ client.java
@@ -2,6 +2,7 @@
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 public class client {
     public static void main(String[] args) {
@@ -11,11 +12,21 @@ public class client {
             boolean end = false;
             handshake(s);
    
+//Tells the server that the client is ready for a job
+sendMsg(s, "REDY");
+            //Get the server state information
+         /*   sendMsg(s, "GETS All");
+            //Obtains server state info
+            myString = readMsg(s);
+            System.out.println("Server State Info: " + myString);
+            sendMsg(s, "OK");*/
             //While the simulation has not been told to end
             while(end == false){
+                //Tells the server that the client is ready for a job
+                sendMsg(s, "REDY");
                 //Obtains  job from the server
                 myString = readMsg(s);
-                System.out.println("RCVD: " + myString);
+                System.out.println("JOB: " + myString);
                 //Checks to see if the received command is "NONE" or "QUIT" which
                 //ends the program
                 if(myString == "NONE" || myString == "QUIT"){
@@ -23,8 +34,77 @@ public class client {
                     break;
                 }
                 //myString is now a job that needs to be scheduled
-                //changing end to true here for debugging purposes
-                end = true;
+
+
+                
+
+
+
+
+
+
+
+
+                if(myString.contains("JOBN")){
+                    String[] JOBNSplit = myString.split(" ");
+                    /*
+                    String command = JOBN;
+                    int submitTime = 0;
+                    int jobID = 0;
+                    int estRuntime = 0;
+                    int core = 0;
+                    int memory = 0;
+                    int disk = 0;
+                    */
+
+                    
+                    //See what servers are available
+                    sendMsg(s, "GETS Avail " + JOBNSplit[4] + " " + JOBNSplit[5] + " " + JOBNSplit[6]);
+                    myString = readMsg(s);
+                    System.out.println("Data: " + myString);
+                    //Say OK to the server (ie: OK i got your msg)
+                    sendMsg(s, "OK");
+
+                    //Read the available servers data
+                    myString = readMsg(s);
+                    System.out.println("After Avail: " + myString);
+
+
+                    String[] AvailSplit = myString.split(" ");
+                    sendMsg(s, "OK");
+                    if(AvailSplit.length <= 0){
+                        break;
+                    }
+                    
+                    myString = readMsg(s);
+                    System.out.println("After Avail: " + myString);
+                    if(myString == "."){
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+                    //Schedules the job
+                    sendMsg(s, "SCHD " + JOBNSplit[2] + AvailSplit[0] + " " + AvailSplit[1]);
+                    myString = readMsg(s);
+                    System.out.println("SCHD: " + myString);
+                }
+                if(myString.contains("DATA")){
+                    sendMsg(s, "OK");
+                }
+                else if(myString.contains("NONE")){
+                    end = true;
+                    break;
+                }
+
+                
+
+
+
+
+
+
+
+
+
+
             }
 
             //Sends "Quit" to the server to end the session
@@ -36,7 +116,7 @@ public class client {
         }
     }
 
-    public static String readMsg(Socket s){
+    public static synchronized String readMsg(Socket s){
         String myString = "FAIL";
         try {
             DataInputStream dis = new DataInputStream(s.getInputStream());
@@ -59,7 +139,7 @@ public class client {
         
     }
 
-    public static void sendMsg(Socket s, String myString){
+    public static synchronized void sendMsg(Socket s, String myString){
         try {
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             byte[] byteArray = myString.getBytes();
@@ -82,13 +162,10 @@ public class client {
         System.out.println("RCVD: " + myString);
 
         //Authenticate with a username (ubuntu)
-        sendMsg(s, "AUTH ubuntu");
+        sendMsg(s, "AUTH " + System.getProperty("user.name"));
 
         //Check to see if sever has ok'd the client's AUTH
         myString = readMsg(s);
         System.out.println("RCVD: " + myString);
-
-        //Tells the server that the client is ready to start taking jobs
-        sendMsg(s, "REDY");
     }
 }
